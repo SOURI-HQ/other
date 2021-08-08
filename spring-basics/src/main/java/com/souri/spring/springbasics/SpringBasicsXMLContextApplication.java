3e1d212ac0ab963887f9d0dc180b736f8803a770
SOURI-HQ <seconddan@wp.pl>
2021-08-08 16:03:16 +0200

    mixed XML context with component scan for beans defined with annotations

--- spring-basics/src/main/java/com/souri/spring/springbasics/SpringBasicsXMLContextApplication.java
+++ spring-basics/src/main/java/com/souri/spring/springbasics/SpringBasicsXMLContextApplication.java
@@ -3,18 +3,16 @@ package com.souri.spring.springbasics;
 import com.souri.spring.springbasics.xml.XmlPersonDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan
 public class SpringBasicsXMLContextApplication {
 
 	private static Logger LOGGER = LoggerFactory.getLogger(SpringBasicsXMLContextApplication.class);
 
 	public static void main(String[] args) {
 		try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml")) {
+			LOGGER.info("{} beans are being loaded: {}", applicationContext.getBeanDefinitionCount(), applicationContext.getBeanDefinitionNames());
+
 			XmlPersonDAO personDAO = applicationContext.getBean(XmlPersonDAO.class);
 
 			LOGGER.info("{}", personDAO);
